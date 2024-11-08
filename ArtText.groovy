98839b2fcd39fb81ef1a70b4adf3af4d4d022a58
Jansen Smith <jansen@bstarengineering.com>
2024-11-08 18:05:40 -0500

    adding output properties

--- ArtText.groovy
+++ ArtText.groovy
@@ -2,10 +2,10 @@ import com.neuronrobotics.bowlerstudio.BowlerStudioController
 
 import eu.mihosoft.vrl.v3d.*
 
-println javafx.scene.text.Font.getFontNames()
+def piece = "mechEng"
 
 def font = "FreeSerif"
-def size_pts = 30
+def size_pts = 8
 def depth = 5 
 
 def spacing = 2
@@ -23,10 +23,22 @@ CSG WorcFreeInst = CSG.text(WorcFreeInst_string, depth, size_pts, font).movey(sp
 mechEng_string = "Mechanical Engineers, workers of the world"
 CSG mechEng = CSG.text(mechEng_string, depth, size_pts, font).movey(spacing_mm*2)
 
+//if piece = ""
 CSG ret = AAS_credit.union(WorcFreeInst).union(mechEng)
 
-BowlerStudioController.addCsg(ret)
+//BowlerStudioController.addCsg(ret)
 
 ret = ret.mirrorx()
+
+ret = ret.setColor(javafx.scene.paint.Color.DARKRED)
+			.setName(piece)
+			.addAssemblyStep(0, new Transform().movez(5))
+			.setManufacturing({ toMfg ->
+				return toMfg
+						//.rotx(180)// fix the orientation
+						//.toZMin()//move it down to the flat surface
+			})
+
+println javafx.scene.text.Font.getFontNames()
 
 return ret
